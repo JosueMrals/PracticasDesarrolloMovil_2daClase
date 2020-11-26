cd56e39c5510bfcc2c0b73cffa879b858fb1d68a
@Josue <62188147+jshkey@users.noreply.github.com>
2020-11-26 15:50:51 -0600

    Cambio de la clases a la estructura anterior

--- persona.kt
+++ persona.kt
@@ -1,15 +1,18 @@
-class Persona{
-    var nombre:String = ""
-    var apellido:String = ""
-    constructor(){
+// class Persona{
+//     var nombre:String = ""
+//     var apellido:String = ""
+//     constructor(){
 
-    }
-    constructor(n:String, a:String):this()
-    {
-        nombre=n
-        apellido=a
-    }
-}
+//     }
+//     constructor(n:String, a:String):this()
+//     {
+//         nombre=n
+//         apellido=a
+//     }
+// }
+
+class Persona(var nombre:String, var apellido:String)
+
 fun main(){
     var persona = Persona("Yesser","Miranda")
     var nueva = Persona()
